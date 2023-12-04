05e7716f6e5bd8ca01ef40ff962e9f1f32cf2b5c
mobots <mobots@Ubuntu-Mobots.myguest.virtualbox.org>
2023-12-04 17:18:52 +0100

    ça marche avec les boutons de la Core2!

--- catch/main.py
+++ catch/main.py
@@ -1,52 +1,49 @@
 import M5
 import time
 from machine import Pin, PWM
-from M5 import Widgets
+from M5 import Widgets, BtnA, BtnC
 
 
 SERVO_PIN = 32
 FREQ = 50
-MIN_ANGLE, MAX_ANGLE = 0, 180
-RESOLUTION = 1024
+# MIN_ANGLE, MAX_ANGLE = 0, 180
+# RESOLUTION = 1024
 
 # MIN_PULSE_MS, MAX_PULSE_MS = 1, 2
 # MIN_DUTY = int(MIN_PULSE_MS / 1000 * FREQ * RESOLUTION)
 # MAX_DUTY = int(MAX_PULSE_MS / 1000 * FREQ * RESOLUTION)
-MIN_DUTY = 25
+MIN_DUTY = 33
 MAX_DUTY = 88
 
 PULSE_DELAY = 0.025
 STEP = 1
 
-label = None
+label_close = None
+label_open = None
 servo = None
 
 last_pulse = MIN_DUTY
 
 
+BG_COLOR = 0x222222
+WHITE = 0xffffff
+GREY = 0x666666
+
 def setup():
-  global label, servo
+  global label_close, label_open, servo
 
   servo = PWM(Pin(SERVO_PIN), FREQ)
   M5.begin()
-  Widgets.fillScreen(0x222222)
-  label = Widgets.Label("ON", 104, 79, 1.0, 0xffffff, 0x222222, Widgets.FONTS.DejaVu72)
+  Widgets.fillScreen(BG_COLOR)
+  label_close = Widgets.Label("Close", 20, 200, 1.0, WHITE, BG_COLOR, Widgets.FONTS.DejaVu24)
+  label_open = Widgets.Label("Open", 220, 200, 1.0, WHITE, BG_COLOR, Widgets.FONTS.DejaVu24)
+
 
 def set_pulse(pulse: int):
   global last_pulse
-  print(f"Set pulse to {pulse}")
+#   print(f"Set pulse to {pulse}")
   servo.duty(pulse)
   last_pulse = pulse
-
-
-# def compute_pulse(angle: int):
-#   global label, servo
-#   pulse = int((angle - MIN_ANGLE) / (MAX_ANGLE - MIN_ANGLE) * (MAX_DUTY - MIN_DUTY) + MIN_DUTY)
-#   return pulse
-# 
-# def go_to_angle(angle: int, delay: int = PULSE_DELAY, step: int = STEP):
-#   target_pulse = compute_pulse(angle)
-#   go_to_pulse(target_pulse, delay=delay, step=step)
 
 def go_to_pulse(target_pulse: int, delay: int = PULSE_DELAY, step: int = STEP):
   global last_pulse
@@ -57,15 +54,60 @@ def go_to_pulse(target_pulse: int, delay: int = PULSE_DELAY, step: int = STEP):
     time.sleep(delay)
 
 def open(delay: int = PULSE_DELAY, step: int = STEP):
+  print("Opening...")
   go_to_pulse(MIN_DUTY, delay=delay, step=step)
+  print("Opened!")
 
 def close(delay: int = PULSE_DELAY, step: int = STEP):
+  print("Closing...")
   go_to_pulse(MAX_DUTY, delay=delay, step=step)
+  print("Closed!")
 
+def handle_close_pressed():
+  global label_close
+  label_close.setColor(GREY, BG_COLOR)
+  while not BtnA.isReleased():
+    M5.update()
+  
+  label_close.setColor(WHITE, BG_COLOR)
+  close()
+
+def handle_open_pressed():
+  global label_open
+  label_open.setColor(GREY, BG_COLOR)
+  while not BtnC.isReleased():
+    M5.update()
+  
+  label_open.setColor(WHITE, BG_COLOR)
+  open()
 
 def loop():
-  global label
+  global label_close
   M5.update()
+
+  if BtnA.isPressed():
+    print("Close is pressed")
+    handle_close_pressed()
+
+  if BtnC.isPressed():
+    print("Open is pressed")
+    handle_open_pressed()
+
+#   if BtnA.isReleased():  # easy
+#     print("isReleased")
+#   if BtnA.wasClicked():
+#     print("wasClicked")
+
+#   if BtnA.wasPressed():
+#     print("wasPressed")
+#   if BtnA.wasReleased():
+#     print("wasReleased")
+
+#   if M5.BtnA.wasClicked():
+#     close()
+
+#   if M5.BtnC.wasClicked():
+#     open()
 
 
 
@@ -73,8 +115,8 @@ def loop():
 if __name__ == "__main__":
   try:
     setup()
-    # while True:
-    #   loop()
+    while True:
+      loop()
   except (Exception, KeyboardInterrupt) as e:
     from utility import print_error_msg
     print_error_msg(e)
