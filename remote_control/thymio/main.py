9f8447b1ce80127fa08d52303946e665228b5ee8
mobots <mobots@Ubuntu-Mobots.myguest.virtualbox.org>
2023-12-08 17:02:13 +0100

    smoothed Thymio movements, deactivated leds

--- remote_control/thymio/main.py
+++ remote_control/thymio/main.py
@@ -1,46 +1,39 @@
-from thymio import LEDS_CIRCLE
-import thymio
 import json
+import thymio
 import time
-from umqtt.simple import MQTTClient
 from config import *
+from umqtt.simple import MQTTClient
 
-ACC_THRESHOLD = 0.2
+
+ACC_THRESHOLD = 0.1
+MAX_ACC = 1.0
 MAX_SPEED = 750
 
 thymio.disable_behaviors()
-leds = [LEDS_CIRCLE(i) for i in range(8)]
+leds = [thymio.LEDS_CIRCLE(i) for i in range(8)]
 motors = thymio.MOTORS()
 
+def sign(x):
+    return (x > 0) - (x < 0)
 
-def handle_acceleration(acc_x: float, acc_y: float):
-    led_values = [0 for _ in range(8)]
+def map_acc_to_speed(acc: float, max_speed: int):
+    return sign(acc) * (abs(acc) - ACC_THRESHOLD) / (MAX_ACC - ACC_THRESHOLD) * max_speed
+
+
+def set_motors_speed(acc_x: float, acc_y: float):
     front_speed, turn_speed = 0, 0
 
     # Y-Axis (front)
-    if acc_y > ACC_THRESHOLD:
-        led_values[4] = acc_y
-    elif acc_y < -ACC_THRESHOLD:
-        led_values[0] = - acc_y
-    
     if abs(acc_y) > ACC_THRESHOLD:
-        front_speed = - acc_y * MAX_SPEED
+        front_speed = - map_acc_to_speed(acc_y, max_speed=MAX_SPEED)
+        print(f"{front_speed=}")
 
     # X-Axis (side)
-    if acc_x > ACC_THRESHOLD:
-        led_values[2] = acc_x
-    elif acc_x < -ACC_THRESHOLD:
-        led_values[6] = - acc_x
-
     if abs(acc_x) > ACC_THRESHOLD:
-        turn_speed = acc_x * MAX_SPEED / 2
+        turn_speed = map_acc_to_speed(acc_x, max_speed=MAX_SPEED//2)
         # Invert turn speed if going backwards
         if front_speed < 0:
             turn_speed = - turn_speed
-
-    # Set LEDs
-    for i in range(8):
-        leds[i].intensity(round(10 * led_values[i]))
 
     # Set Motors
     left_speed = int(front_speed + turn_speed)
@@ -48,6 +41,24 @@ def handle_acceleration(acc_x: float, acc_y: float):
     # print(f'Setting motors speed: {left_speed=}, {right_speed=}')
     motors.set_speed(left_speed, right_speed)
 
+def set_leds(acc_x: float, acc_y: float):
+    led_values = [0 for _ in range(8)]
+    
+    # Y-Axis (front)
+    if acc_y > ACC_THRESHOLD:
+        led_values[4] = acc_y
+    elif acc_y < -ACC_THRESHOLD:
+        led_values[0] = - acc_y
+    
+    # X-Axis (side)
+    if acc_x > ACC_THRESHOLD:
+        led_values[2] = acc_x
+    elif acc_x < -ACC_THRESHOLD:
+        led_values[6] = - acc_x
+
+    # Set LEDs
+    for i in range(8):
+        leds[i].intensity(round(10 * led_values[i]))
 
 def mqtt_callback(topic, msg):
     topic_str = topic.decode()
@@ -57,7 +68,8 @@ def mqtt_callback(topic, msg):
     if topic_str == IMU_TOPIC:
         acc = json.loads(msg_decoded)
 
-        handle_acceleration(acc.get("x"), acc.get("y"))
+        set_motors_speed(acc.get("x"), acc.get("y"))
+        # set_leds(acc.get("x"), acc.get("y"))
 
 def connect_to_mqtt():
     global client
@@ -77,6 +89,8 @@ def main():
     global client
 
     connect_to_mqtt()
+
+    print('\nThymio is ready!\n')
 
     try:
         while True:
